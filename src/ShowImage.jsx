b354e4bae70cf1b2878bfc543b0c62778b68bc7c
bhavnaa01 <bhavnaraut0122@gmail.com>
2024-10-20 13:14:24 +0530

    2nd changes

--- src/ShowImage.jsx
+++ src/ShowImage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ShowImage = () => {
   // This is the URL of the image hosted in the public folder
-  const imageUrl = 'https://wee-bhavna-rauts-projects-d60823a4.vercel.app/WE.mp4';  // Update to your production URL when deploying
+  const imageUrl = 'https://wee-bhavna-rauts-projects-d60823a4.vercel.app/natureeee.jpeg';  // Update to your production URL when deploying
   return (
     <div>
       <h1>Here is the Image:</h1>
